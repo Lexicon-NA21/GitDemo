34351dae47fe8514aa26c281dfe5c025b2921d88
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-03-09 11:24:28 +0100

    Commit Konflikt

--- GitDemo/NewFileDimitris.cs
+++ GitDemo/NewFileDimitris.cs
@@ -10,7 +10,7 @@ namespace GitDemo
     {
         public NewFileDimitris()
         {
-
+            Console.WriteLine("Hej David!");
         }
     }
 }
--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -6,7 +6,7 @@ namespace GitDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World from Dimitris");
             Console.WriteLine("From My branch");
             Console.WriteLine("From My branch");
             Console.WriteLine("From My branch");
